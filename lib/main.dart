243fb5994256b683cf5d76f7c89dcc9bb61e2949
yorifuji <yorifuji@gmail.com>
2025-01-19 13:17:28 +0900

    simplify person instantiation and add toJson output

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:freezer/person.dart';
 
 void main(List<String> arguments) {
-  var person = Person('John', 30);
-  person.showOutput();
-
+  Person('John', 30).showOutput();
   Person.fromJson({"name": "Jane", "age": 25}).showOutput();
+  print(Person('John', 30).toJson());
 }
