cbf3697acf28efde4a121870de867acddd5cfb5c
yorifuji <yorifuji@gmail.com>
2025-01-19 13:01:44 +0900

    fvm dart create freezer

---
+++ bin/freezer.dart
@@ -0,0 +1,5 @@
+import 'package:freezer/freezer.dart' as freezer;
+
+void main(List<String> arguments) {
+  print('Hello world: ${freezer.calculate()}!');
+}
---
+++ test/freezer_test.dart
@@ -0,0 +1,8 @@
+import 'package:freezer/freezer.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
